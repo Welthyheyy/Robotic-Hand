f10c293566985666e1cf286ba9be943453f5466f
Welthyheyy <107375493+Welthyheyy@users.noreply.github.com>
2025-12-25 14:39:12 -0500

    Add support for multiple servos in hand movement

--- hand_movement.ino
+++ hand_movement.ino
@@ -7,8 +7,15 @@ AccelStepper stepper(
 );
 
 Servo indexServ;
+Servo middleServ;
+Servo thumbServ;
+Servo ringPinkyServ;
+
 
 int indexPin = 9;
+int middlePin = idk;
+int thumbPin = idk;
+int ringPinkyPin = idk;
 
 int targetServoAngle = 90;
 long targetStepperPos = 0;
@@ -17,23 +24,45 @@ void setup() {
   Serial.begin(9600);
 
   indexServ.attach(indexPin);
+  middleServ.attach(middlePin);
+  thumbServ.attach(thumbPin);
+  ringPinkyServ.attach(ringPinkyPin);
+
   indexServ.write(targetServoAngle);
+  middleServ.write(targetServoAngle);
+  thumbServ.write(targetServoAngle);
+  ringPinkyServ.write(targetServoAngle);
 
   stepper.setMaxSpeed(600);
   stepper.setAcceleration(300);
 }
 
 void loop() {
-  // 🔹 Always run stepper
+  //  Always run stepper
   stepper.run();
 
-  // 🔹 Handle serial without blocking
+  //  Handle serial without blocking
   if (Serial.available()) {
     String cmd = Serial.readStringUntil('\n');
 
-    if (cmd.startsWith("S:")) {
+    if (cmd.startsWith("I:")) {
       targetServoAngle = constrain(cmd.substring(2).toInt(), 0, 180);
       indexServ.write(targetServoAngle);
+    }
+
+    if (cmd.startsWith("M:")) {
+      targetServoAngle = constrain(cmd.substring(2).toInt(), 0, 180);
+      middleServ.write(targetServoAngle);
+    }
+
+    if (cmd.startsWith("T:")) {
+      targetServoAngle = constrain(cmd.substring(2).toInt(), 0, 180);
+      thumbServ.write(targetServoAngle);
+    }
+
+    if (cmd.startsWith("RP:")) {
+      targetServoAngle = constrain(cmd.substring(2).toInt(), 0, 180);
+      ringPinkyServ.write(targetServoAngle);
     }
 
     if (cmd.startsWith("W:")) {
